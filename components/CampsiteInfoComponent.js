1ff5e531b0cf0f9287be3a952245c260f865da1f
Doug S <sorged91@gmail.com>
2021-09-11 12:18:16 -0400

    swiper gestures updated

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -38,6 +38,7 @@ function RenderCampsite(props) {
   const view = React.createRef();
 
   const recognizeDrag = ({ dx }) => (dx < -200 ? true : false);
+  const recognizeComment = ({ dx }) => (dx > 200 ? true : false);
 
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
@@ -71,6 +72,8 @@ function RenderCampsite(props) {
           ],
           { cancelable: false }
         );
+      } else if (recognizeComment(gestureState)) {
+        props.onShowModal();
       }
       return true;
     },
